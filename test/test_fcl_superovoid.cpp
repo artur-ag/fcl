5d99cfdc530a7e7e7267b04d93aee968d1ab464c
Artur Gonçalves <artur.alves.goncalves@gmail.com>
2016-04-18 23:24:18 +0900

    Increased superovoid test battery to 10000 tests.

--- test/test_fcl_superovoid.cpp
+++ test/test_fcl_superovoid.cpp
@@ -1091,7 +1091,7 @@ BOOST_AUTO_TEST_CASE(mubo_implicit_vs_parametric_same_guess)
 	MuboPoseGenerator rand = MuboPoseGenerator();
 	rand.setSeed(1337);
 
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < 10000; i++)
 	{
 		rand.randomizeSuperovoid(sov1);
 		rand.randomizeSuperovoid(sov2);
